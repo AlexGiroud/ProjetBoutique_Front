9687e4ab96df94b5972530b4dbbd09431855a111
Alexandre Vladovich <alexandre@vladovich.com>
2017-02-02 23:48:54 +0100

    Add badge to basket

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,30 @@
 angular.module('starter.controllers', [])
 
-.controller('ShopCtrl', function($scope, $http, basketService, ) {
+.controller('ShopCtrl', function($scope, $http, basketService, $ionicPopup, $rootScope) {
 
   $http.get("http://giroud-bit.fr:3000/api/produits").success(function(data){
     $scope.products = data;
   })
 
   $scope.addToBasket = function(product) {
-    basketService.basketProducts.push(product);
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Ajouter au panier',
+      template: 'Êtes-vous sûr de vouloir ajouter cet article à votre panier ?',
+      cancelText: 'Annuler',
+      okText: 'Oui'
+    });
+
+    confirmPopup.then(function(res) {
+      if(res) {
+        basketService.basketProducts.push(product);
+        $rootScope.$broadcast('update');
+        var alertPopup = $ionicPopup.alert({
+          title: 'Ajouté',
+          template: 'Produit ajouté au panier'
+        });
+      }
+    });
   }
-  
 })
 
 .controller('CartCtrl', function($scope, basketService) {
@@ -17,7 +32,7 @@ angular.module('starter.controllers', [])
 
   $scope.amountBasket = function() {
     var amount = 0;
-    for(product in $basket.products) {
+    for(product in $scope.products) {
       amount += product.price;
     }
     return amount;
@@ -27,6 +42,14 @@ angular.module('starter.controllers', [])
 .controller('ProductCtrl', function($scope, $stateParams, $http) {
   $http.get("http://giroud-bit.fr:3000/api/produits/"+$stateParams['productID']).success(function(data) {
     $scope.product = data;
+  })
+})
+
+.controller('TabCtrl', function($scope, basketService) {
+  $scope.$on('update',function(){
+    $scope.data = {
+      badge : basketService.basketProducts.length
+    };
   })
 })
 
